5eb0cde003673b36ce69d11a0cec6568d2ca5eae
yl <1195455487@qq.com>
2020-12-08 11:40:02 +0800

    1.0

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelpDoc ></HelpDoc>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelpDoc from './components/HelpDoc.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelpDoc
   }
 }
 </script>
@@ -19,8 +17,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
